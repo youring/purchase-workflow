eef6331097f9ccac57eee175c32150d128ce9417
aheficent <ahenriquez@eficent.com>
2018-01-11 16:36:52 +0100

    [IMP]migrations scripts purchase_request v8

---
+++ purchase_request/migrations/8.0.1.1.0/pre-migration.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Eficent <http://www.eficent.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openupgradelib import openupgrade
+
+
+column_copies = {
+    'purchase.request': [
+        ('warehouse_id', None, None),
+    ],
+}
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.copy_columns(env.cr, column_copies)
